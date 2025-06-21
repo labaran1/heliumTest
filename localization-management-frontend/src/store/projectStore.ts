8d2ba27266d4dff2e77d4b4b7e3ef65a2ac6fa6d
labaran1 <adlabaran@gmail.com>
2025-06-21 21:20:04 +0000

    project store

--- localization-management-frontend/src/store/projectStore.ts
+++ localization-management-frontend/src/store/projectStore.ts
@@ -1,9 +1,28 @@
 import { create } from 'zustand';
-import { ProjectStore } from "../types/projects";
+import { ProjectStore, Project } from "../types/projects";
+
+// Sample projects
+const sampleProjects: Project[] = [
+    {
+      id: '1',
+      name: 'Web App',
+      description: 'Web application translations',
+    },
+    {
+      id: '2',
+      name: 'Mobile App',
+      description: 'Mobile application translations',
+    },
+    {
+      id: '3',
+      name: 'Marketing Site',
+      description: 'Marketing website translations',
+    },
+  ];
 
 
 export const useProjectStore = create<ProjectStore>((set) => ({
-    projects: [],
+    projects: sampleProjects,
     actions: {
         addProject: (project) => set((state) => ({
             projects: [...state.projects, project],
