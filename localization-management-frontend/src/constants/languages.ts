5c3392c838fff6df1bc0b2a887ff124dcf8ecb9a
labaran1 <adlabaran@gmail.com>
2025-06-22 01:03:11 +0000

    langauge constant

---
+++ localization-management-frontend/src/constants/languages.ts
@@ -0,0 +1,11 @@
+import { LanguageMap } from "../types/translation";
+
+
+export const AVAILABLE_LANGUAGES: LanguageMap[] = [
+    { code: 'en', label: 'English', flag: '🇬🇧' },
+    { code: 'fr', label: 'French', flag: '🇫🇷' },
+    { code: 'es', label: 'Spanish', flag: '🇪🇸' },
+    { code: 'de', label: 'German', flag: '🇩🇪' },
+    { code: 'zh', label: 'Chinese', flag: '🇨🇳' },
+    { code: 'ar', label: 'Arabic', flag: '🇸🇦' },
+  ];
