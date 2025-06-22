b407532be6b87715c45310294bb740aeadf1c94a
labaran1 <adlabaran@gmail.com>
2025-06-22 22:10:54 +0000

    layout

--- localization-management-frontend/app/layout.tsx
+++ localization-management-frontend/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+import QueryProvider from "@/src/store/Providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +24,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+ 
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <QueryProvider>
+          {children}
+          </QueryProvider>
       </body>
     </html>
   );
